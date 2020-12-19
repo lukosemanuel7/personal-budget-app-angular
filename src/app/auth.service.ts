16f5622ddda20ba820c00b1178ee5e7da86fcd05
lukosemanuel7 <lukosemanuel7@gmail.com>
2020-12-18 23:14:16 -0500

    fixed server address issue

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -20,7 +20,7 @@ export class AuthService {
   user: any;
   tokenSubscription = new Subscription()
   timeout;
-  apiUrl = 'http://64.225.52.181/:3000/';
+  apiUrl = 'http://64.225.52.181:3000/';
 
   constructor(private router: Router, private http: HttpClient) {
     this.userLoggedIn.next(false);
